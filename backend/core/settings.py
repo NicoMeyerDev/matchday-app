f11ddee6704aed6f9cbf70ae176fbb54ff59246f
Nico <BuggsbyN@gmail.com>
2026-06-15 19:17:19 +0200

    fix: WHITENOISE_ROOT setzen damit /assets/ Dateien korrekt ausgeliefert werden

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -85,6 +85,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR.parent / 'frontend' / 'dist']
+WHITENOISE_ROOT = BASE_DIR.parent / 'frontend' / 'dist'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
