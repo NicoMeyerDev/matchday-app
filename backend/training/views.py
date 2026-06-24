1953b2a730ee543457e74baec2c81a108214b104
BuggsbyN <2025nico@gmail.com>
2026-06-24 15:14:40 +0200

    Automatisches erstellen der Reihenfolge bei Auswahl Training = Klassisch

--- backend/training/views.py
+++ backend/training/views.py
@@ -11,6 +11,15 @@ class TrainingViewSet(ModelViewSet):
 
     def get_queryset(self):
         return Training.objects.filter(club__owner=self.request.user)
+    
+    def perform_create(self, serializer):
+        training = serializer.save()
+        if training.trainingsart == 'klassisch':
+            TrainingsBlock.objects.create(training = training, name = 'Aktivierung/Erwärmung' , trainingstyp = 'aktivierung', reihenfolge = 1)
+            TrainingsBlock.objects.create(training = training, name = 'Spielform_1', trainingstyp = 'spielform_1', reihenfolge = 2)
+            TrainingsBlock.objects.create(training = training, name = 'Zwischenblock', trainingstyp = 'zwischenblock', reihenfolge = 3)
+            TrainingsBlock.objects.create(training = training, name = 'SPielform_2', trainingstyp = 'spielform_2', reihenfolge = 4)
+
 
 class TrainingsBlockViewSet(ModelViewSet):
     serializer_class = TrainingsBlockSerializer
