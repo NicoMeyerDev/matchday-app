ace03305ae6fa8d35fe3684efd12d3a26443765e
BuggsbyN <2025nico@gmail.com>
2026-06-28 13:10:50 +0200

    PostgreSQL Umstellung: dj-database-url, settings angepasst, gitignore bereinigt

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -8,6 +8,7 @@ Diese Konfiguration ist bewusst einfach gehalten:
 """
 from datetime import timedelta
 from pathlib import Path
+import dj_database_url
 import os
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -73,10 +74,10 @@ TEMPLATES = [
 WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=f'sqlite:///{BASE_DIR / "db.sqlite3"}',
+        conn_max_age=600,
+    )
 }
 
 LANGUAGE_CODE = 'de-de'
